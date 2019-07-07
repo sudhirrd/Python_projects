701e5c5cfbf5fa40143b46ed142994d29367a50e
Sudhir Dhameliya <sudhirrdhameliya@gmail.com>
2019-07-07 12:15:11 +0530

    Update 3 csv_to_database.py

--- 3 csv_to_database.py
+++ 3 csv_to_database.py
@@ -1,6 +1,7 @@
 import MySQLdb
+import csv
 
-def solution(host, user, password, databse,filename):
+def solution(host, user, password, databse, tablename, filename):
     my_db = MySQLdb.connect(
         host=host,
         user=user,
@@ -11,18 +12,20 @@ def solution(host, user, password, databse,filename):
 
     with open(filename, "r") as file:
         col_numbers = file.readline().split(",")
+        col_numbers_1 = file.readline().strip("\n").split(",")
 
-    if (col_numbers[0].isnumeric()):
-        my_cursor.execute("CREATE TABLE temp_table({} INT(10)".format(col_numbers[0]))
-    else:
-        my_cursor.execute("CREATE TABLE temp_table({} VARCHAR(255)".format(col_numbers[0]))
+        col_types = ["INT(10)" if val.isnumeric() else "VARCHAR(255)" for val in col_numbers_1]
 
-    del col_numbers[0]
-    for val in col_numbers:
-        if(val.isnumeric()):
-            my_cursor.execute("ALTER TABLE temp_table ADD {} INT(10)".format(val))
-        else:
-            my_cursor.execute("ALTER TABLE temp_table ADD {} VARCHAR(255)".format(val))
+        query = "CREATE TABLE "+tablename+"("
+        for val_1, val_2 in zip(col_numbers, col_types):
+            query += "{} {},".format(val_1, val_2)
+        query = query[:-1] + ")"
 
+        my_cursor.execute(query)
 
-# solution("localhost", "root", "", "imdb_db", "temp.csv")
+        reader = csv.reader(file)
+        next(reader)
+        my_cursor.execute("LOAD DATA LOCAL INFILE '"+filename+"' INTO TABLE "+tablename+" FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS")
+        my_db.commit()
+
+# solution("localhost", "root", "", "imdb_db", "temp_table", "imdb.csv")
